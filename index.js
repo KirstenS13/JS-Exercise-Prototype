e3e44b3003b3444b6eaa8f7f9c47282a2acfa5ba
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-03-16 22:00:58 -0400

    began working on Task 1, the Person constructor

--- index.js
+++ index.js
@@ -39,8 +39,14 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age, stomach) {
+  this.name = name;
+  this.age = age;
+  this.stomach = stomach[];
+}
 
+Person.prototype.eat = function(someFood) {
+  return this.stomach.push(someFood);
 }
 
 /*
